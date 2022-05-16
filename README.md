157b1b8e97981fb2dd00c22388ab8a45b149940a
LukeTheCodeMaker <luke.honeyball.03@gmail.com>
2022-05-16 21:12:11 +0200

    added the project
    I added the solution and the readme.md.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # a-basic-vpn
  A SUPER SMALL vpn in a console app for windows.
+
+Just type YES, to connect to a dutch server, and NO to disconnect.
---
+++ proxy/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+namespace proxy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Title = "VPN";
+            Console.Write("Enable vpn or not? (Yes/No):");
+            
+            string op = Console.ReadLine();
+            RegistryKey key = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings", true);
+            if (op.ToLower() == "yes")
+            {
+                string ip = "217.195.153.126";
+                string port = "3128";
+                
+                key.SetValue("ProxyEnable", 1);
+                key.SetValue("ProxyServer", ip + ":" + port);
+                Console.WriteLine("connecting.. Press enter to continue");
+                Console.ReadLine();
+            }
+            else {
+                key.SetValue("ProxyEnable", 0);
+                Console.WriteLine("disabled the vpn");
+                Console.ReadLine();
+            }
+
+            
+            
+        }
+    }
+}
